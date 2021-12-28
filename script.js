def9134294d4c0aac8aaa3dd653ec638b64d65ec
ashleysalinas <ashleysalinas98@protonmail.com>
2021-12-28 15:57:19 -0600

    Questions display correctly

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ var timerCount = 60;
 var counter = 0;
 var scoreCounter = 0;
 
+
 var questionsList = [
     {name: "Which of the following is NOT a language used for building pages?",
     choices: ["HTML", "CSS", "SQL", "Javascript"],
@@ -39,6 +40,8 @@ function startGame() {
     //introCard.style.visibility = "hidden";
     //Displays question
     displayQuestion();
+
+    startTimer();
 };
 
 //Creates countdown timmer
@@ -52,30 +55,41 @@ function startTimer() {
     }, 1000)
 }
 
+let currentQuestion = questionsList[counter];
 function displayQuestion() {
-    startButton.style.visibility = "hidden"
-    startTimer();
-    var currentQuestion = questionsList[counter];
+    startButton.style.visibility = "hidden";
     question.textContent = currentQuestion.name;
+    answerCard.innerHTML = "" //clears out old answer choices
     currentQuestion.choices.forEach(function(item) {
             var answerButton = document.createElement("Button");
             answerButton.textContent = item;
             answerButton.setAttribute("value", answerButton.textContent);
             answerCard.append(answerButton);
-            answerButton.addEventListener("click", answerClick);
+            answerButton.addEventListener("click", displayNextQuestion)
     })
 }
-function answerClick(answerButton) {
-    answerButton.preventDefault();
-    if (answerButton.value === question.answer) {
+function displayNextQuestion(button) {
+    if (this.value === currentQuestion.answer) {
         scoreCounter++; //Adds to score count upon clicking correct answer
         scoreCard.textContent = "Score: " + scoreCounter;
         localStorage.setItem("scoreCount", scoreCounter);
     } else {
         timerCount -= 5; //Takes 5 seconds away for every wrong answer
         scoreCounter--; //Takes away points for incorrect answer
+        if (scoreCounter < 0) {
+            scoreCounter = 0 //prevents negative scores
+        }
         scoreCard.textContent = "Score: " + scoreCounter;
         localStorage.setItem("scoreCount", scoreCounter);
     }
     counter++
+    console.log(counter)
+    currentQuestion = questionsList[counter];
+    if (counter === questionsList.length) {
+        console.log("game over")
+    } else {
+        new displayQuestion(currentQuestion)
+    }
 }
+
+
